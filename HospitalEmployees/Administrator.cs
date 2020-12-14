a340d524e252dcc22f7b3553e5985c710f0904e4
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2020-12-14 19:20:34 +0100

    finish lib

---
+++ HospitalEmployees/Administrator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HospitalEmployees
+{
+    public class Administrator: Employee
+    {
+        public readonly bool Admin = true;
+
+        public Administrator(string name, string surname, int PESEL, string login, string password) :
+                base(name, surname, PESEL, login, password)
+        {
+        }
+    }
+}
--- HospitalEmployees/Nurse.cs
+++ HospitalEmployees/Nurse.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace HospitalEmployees
 {
     public class Nurse: Employee
     {
-        public Nurse()
+        public List<int> Shifts
         {
+            get
+            {
+                Shifts.Sort();
+                return Shifts;
+            }
+            set
+            {
+                if (Shifts.Count > 10)
+                {
+                    throw new Exceptions.TooManyShiftsException();
+                }
+            }
+        }
+
+        public Nurse(string name, string surname, int PESEL, string login, string password) :
+                base(name, surname, PESEL, login, password)
+        {
+        }
+
+        public void AddShift(int day)
+        {
+            if (!Shifts.Contains(day) && Shifts.Count < 10)
+            {
+                Shifts.Add(day);
+                Shifts.Sort();
+            }
+            else
+            {
+                throw new Exceptions.AddShiftFailureException();
+            }
+        }
+
+        public void RemoveShift(int index)
+        {
+            Shifts.Remove(index);
+            Shifts.Sort();
         }
     }
 }
