8f33a2a812024bf77bfcea12813e0dab5fb3e4d1
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2021-01-02 20:28:10 +0100

    login logic checks

--- HospitalApp/Submenus/AdminCreationSubmenu.cs
+++ HospitalApp/Submenus/AdminCreationSubmenu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace HospitalApp.Submenus
 {
@@ -21,6 +22,7 @@ namespace HospitalApp.Submenus
 
                 if (select == 1)
                 {
+                    int check = 0;
                     Console.Clear();
                     Console.WriteLine("--- Add nurse ---\n");
                     Console.WriteLine("Enter name:\n");
@@ -34,11 +36,36 @@ namespace HospitalApp.Submenus
                     Console.WriteLine("Enter password:");
                     string password = Console.ReadLine();
 
-                    data.AddNurse(name, surname, PESEL, login, password);
-                    data.DataExport();
+                    foreach (var item in data.Nurses)
+                    {
+                        if (name != item.Name && surname != item.Surname)
+                        {
+                            check++;
+                        }
+                        if (PESEL != item.PESEL)
+                        {
+                            check++;
+                        }
+                        if (loginUniquity(login))
+                        {
+                            check++;
+                        }
+                    }
+
+                    if (check == (3 * data.Nurses.Count))
+                    {
+                        data.AddNurse(name, surname, PESEL, login, password);
+                        data.DataExport();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Adding new nurse has failed. Please try again.");
+                    }
+                            
                 }
                 else if (select == 2)
                 {
+                    int check = 0;
                     Console.Clear();
                     Console.WriteLine("--- Add doctor ---\n");
                     Console.WriteLine("Enter name:\n");
@@ -55,12 +82,54 @@ namespace HospitalApp.Submenus
                     int pwz = int.Parse(Console.ReadLine());
                     Console.WriteLine("Enter specialization");
                     string spec = Console.ReadLine();
+                    if (spec.ToLower() == "cardiologist" || spec.ToLower() == "urologist"
+                        || spec.ToLower() == "neurologist" || spec.ToLower() == "laryngologist")
+                    {
+                        foreach (var item in data.Doctors)
+                        {
+                            if (name != item.Name && surname != item.Surname)
+                            {
+                                check++;
+                            }
+                            if (PESEL != item.PESEL)
+                            {
+                                check++;
+                            }
+                            if (loginUniquity(login))
+                            {
+                                check++;
+                            }
+                            if (pwz != item.PWZnumber)
+                            {
+                                check++;
+                            }
+                        }
+                        if (check == (4 * data.Doctors.Count))
+                        {
+                            data.AddDoctor(name, surname, PESEL, login, password, pwz, spec);
+                            data.DataExport();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Adding new doctor has failed. Please try again.");
+                        }
 
-                    data.AddDoctor(name, surname, PESEL, login, password, pwz, spec);
-                    data.DataExport();
+                    }
+                    else
+                    {
+                        Console.WriteLine("There are only 4 available specializations. Please try again with one of them:\n" +
+                            "1. Cardiologist\n" +
+                            "2. Urologist\n" +
+                            "3. Neurologist\n" +
+                            "4. Laryngologist");
+                        Thread.Sleep(1000);
+                    }
+
+
                 }
                 else if (select == 5)
                 {
+                    int check = 0;
                     Console.Clear();
                     Console.WriteLine("--- Add nurse ---\n");
                     Console.WriteLine("Enter name:\n");
@@ -74,8 +143,33 @@ namespace HospitalApp.Submenus
                     Console.WriteLine("Enter password:");
                     string password = Console.ReadLine();
 
-                    data.AddAdmin(name, surname, PESEL, login, password);
-                    data.DataExport();
+                    foreach (var item in data.Administrators)
+                    {
+                        if (name != item.Name && surname != item.Surname)
+                        {
+                            check++;
+                        }
+                        if (PESEL != item.PESEL)
+                        {
+                            check++;
+                        }
+                        if (loginUniquity(login))
+                        {
+                            check++;
+                        }
+                    }
+
+                    if (check == (3 * data.Nurses.Count))
+                    {
+                        data.AddAdmin(name, surname, PESEL, login, password);
+                        data.DataExport();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Adding new admin has failed. Please try again.");
+                    }
+
+
                 }
                 else if (select == 9)
                 {
@@ -89,6 +183,42 @@ namespace HospitalApp.Submenus
                     data.DataExport();
                     new Menu();
                 }
+            }
+        }
+
+        private bool loginUniquity(string login)
+        {
+            int check = 0;
+            int total = data.Nurses.Count + data.Doctors.Count + data.Administrators.Count;
+            foreach (var item in data.Nurses)
+            {
+                if (login != item.login)
+                {
+                    check++;
+                }
+            }
+            foreach (var item in data.Doctors)
+            {
+                if (login != item.login)
+                {
+                    check++;
+                }
+            }
+            foreach (var item in data.Administrators)
+            {
+                if (login != item.login)
+                {
+                    check++;
+                }
+            }
+
+            if (check == total)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
